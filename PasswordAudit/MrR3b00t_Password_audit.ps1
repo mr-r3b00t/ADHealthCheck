966b608f63eaf3827bf211d2f291764dcbd44bc4
Daniel Card <dan.card@xservus.com>
2019-08-15 14:10:06 +0100

    added ncsc password audit tool

---
+++ PasswordAudit/MrR3b00t_Password_audit.ps1
@@ -0,0 +1,8 @@
+﻿#import DSInternals
+
+.\pwauditor -DC vuln.lab.local -NC "dc=vuln,dc=lab,dc=local" -blacklists top_1000.txt,top_1000_mangled.txt,top_10000.txt -organisation PSTG
+
+#run with output all data
+.\pwauditor -DC vuln.lab.local -NC "dc=vuln,dc=lab,dc=local" -blacklists top_1000.txt,top_1000_mangled.txt,top_10000.txt -organisation PSTG -unsuppress 1
+
+
